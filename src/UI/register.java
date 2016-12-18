a66b743c66179c3edd1110a8aa815b676dfc4018
Chun Han <Chun Han>
2016-12-18 23:28:51 +0800

    [#135305073] Register UI page

--- src/UI/register.java
+++ src/UI/register.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package UI;
 
 import java.text.SimpleDateFormat;
@@ -12,10 +7,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author han
- */
 public class register extends javax.swing.JFrame {
 
     /**
