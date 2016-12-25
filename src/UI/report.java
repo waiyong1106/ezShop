2c93358a3bfa039bffac86cf9bcbef13fa654073
inotsucker <weijianl-wa14@student.tarc.edu.my>
2016-12-25 20:18:09 +0800

    Refund report button function coded

--- src/UI/report.java
+++ src/UI/report.java
@@ -114,7 +114,8 @@ public class report extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        
+        dispose();
+        new ReportRefund().setVisible(true);
 
     }//GEN-LAST:event_jButton3ActionPerformed
 
