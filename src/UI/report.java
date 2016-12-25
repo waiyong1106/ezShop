6fecf2ba6a0fc981034427c0220561a1cddfd902
inotsucker <weijianl-wa14@student.tarc.edu.my>
2016-12-25 18:12:33 +0800

    Schedule and Transaction button function added

--- src/UI/report.java
+++ src/UI/report.java
@@ -119,7 +119,8 @@ public class report extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
+        dispose();
+        new ReportTransaction().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
