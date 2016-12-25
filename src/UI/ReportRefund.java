ce7d63e5322399d7acf47adf1184288696c2ea6d
inotsucker <weijianl-wa14@student.tarc.edu.my>
2016-12-25 20:39:53 +0800

    [#136640371] Refund report fixed error and done

--- src/UI/ReportRefund.java
+++ src/UI/ReportRefund.java
@@ -48,6 +48,8 @@ public class ReportRefund extends javax.swing.JFrame {
         }
     }
     
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
